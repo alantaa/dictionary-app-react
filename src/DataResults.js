2906497e7f77a207cf5f56f89110dc862a00b82a
alantaa <102055628+alantaa@users.noreply.github.com>
2022-06-06 21:57:07 +0100

    Phonetics implemented

--- src/DataResults.js
+++ src/DataResults.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./DataResults.css";
 import DataMeaning from "./DataMeaning";
+import Phonetics from "./Phonetics";
 
 export default function DataResults(props) {
   if (props.data) {
     return (
       <div className="DataResults">
         <h2 className="title">{props.data.word}</h2>
+        {props.data.phonetics.map(function (phonetics, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetics={phonetics} />
+            </div>
+          );
+        })}
+
         {props.data.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
---
+++ src/Phonetics.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Phonetics(props) {
+  console.log(props.phonetics);
+  return (
+    <div className="Phonetics">
+      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
+        Listen
+      </a>{" "}
+      {props.phonetics.text}
+    </div>
+  );
+}
