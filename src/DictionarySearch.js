3b959e6bb078c0951191ea637668ec6d33b86ef4
alantaa <102055628+alantaa@users.noreply.github.com>
2022-06-07 18:44:18 +0100

    Pexel photos search API implemented

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -2,19 +2,31 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./DictionarySearch.css";
 import DataResults from "./DataResults";
+import Photos from "./Photos";
 
 export default function DictionarySearch() {
-  const [keyword, setKeyword] = useState("Read");
+  const [keyword, setKeyword] = useState("Code");
   const [results, setResults] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const pexelApiKey =
+      "563492ad6f917000010000010ebf316127104347b12ef3c5a0fb97f7";
+    const pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    const headers = { Authorization: `Bearer ${pexelApiKey}` };
+    axios.get(pexelApiUrl, { headers: headers }).then(handlePexelResponse);
   }
 
   function handleSubmit(event) {
@@ -45,6 +57,7 @@ export default function DictionarySearch() {
           </p>
         </section>
         <DataResults data={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
